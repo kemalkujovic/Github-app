68936119052db4b479545ba6c5900985a85022ad
kemalkujovic <kemalkujovic1111@gmail.com>
2022-10-30 15:45:10 +0100

    Github-app

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 let btn = document.querySelector("#btn");
 let container = document.querySelector(".container");
 btn.addEventListener("click", getUserName);
+let repositories = document.querySelector(".repositories");
+let about = document.querySelector(".about");
 let resData = null;
 function renderUserName(data) {
   let html = `
-          <img class="logo" src="${data.avatar_url}" />
-          <h1 class="naziv">${data.name}</h1>
-          <p1 class="bio">${data.bio}</p1>
-          <p1 onclick="following()" class="naziv followers">${data.followers} followers - ${data.following} following</p1>
-          <p1 class="naziv">Location: ${data.location}</p1>
+  <img class="logo" src="${data.avatar_url}" />
+  <h1 class="naziv">${data.name}</h1>
+  <p1 class="bio">${data.bio}</p1>
+  <p1 onclick="following()" class="naziv followers">${data.followers} followers - ${data.following} following</p1>
+  <p1 class="naziv">Location: ${data.location}</p1>
+  
+  `;
 
-          `;
-  container.innerHTML = "";
-  container.insertAdjacentHTML("beforeend", html);
+  about.innerHTML = "";
+  about.insertAdjacentHTML("beforeend", html);
+}
+
+function renderRepositories(data) {
+  let html = `
+  <button class ="btns">${data.name}</button>
+  `;
+  document.querySelector(".h1-res").style.opacity = 1;
+  repositories.insertAdjacentHTML("beforeend", html);
 }
 
 function getUserName() {
@@ -23,8 +34,15 @@ function getUserName() {
     .then((response) => response.json())
     .then((data) => {
       resData = data;
-      console.log(resData.repos_url);
+      console.log(resData);
       renderUserName(data);
+      return fetch(`https://api.github.com/users/${username}/repos`);
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((link) => {
+        renderRepositories(link);
+      });
     })
     .catch((err) => {
       console.log(err);
